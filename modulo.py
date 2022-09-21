976886c2420edb513b586caf1a5672ef5474f70d
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2022-09-21 17:45:42 -0400

    Implement ModuleField class

---
+++ modulo.py
@@ -0,0 +1,40 @@
+import numpy as np
+class ModuloField:
+    def __init__(self, value) -> None:
+        self.value = value
+        
+
+    def computeMultiplicationTable(self):
+        mod = np.vectorize(lambda x: x % self.value)
+        keys = np.arange(0,self.value)
+        result =  mod(np.outer(keys,keys))
+        r,c = result.shape
+        resultArray = np.ones((r+1,c+1),dtype=int)
+        resultArray[1:,1:] = result
+        resultArray[1:, 0] = keys
+        resultArray[0, 1:] = keys
+        resultArray[0,0] = -1
+        
+
+        return resultArray
+
+    def computeAdditionTable(self):
+        keys = np.arange(0,self.value)
+        resultArray = np.ones((keys.shape[0]+1,)*2, dtype=int)
+        resultArray[1:, 0] = keys
+        resultArray[0, 1:] = keys
+        resultArray[0,0] = -1
+
+
+        for i in range(keys.shape[0]):
+            for j in range(keys.shape[0]):
+                resultArray[i+1, j+1] = (i + j) % self.value
+
+        return resultArray
+
+
+
+
+
+
+print(ModuloField(6).computeMultiplicationTable())
